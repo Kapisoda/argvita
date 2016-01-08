bda8c3a008cef15df2d25aeec78428dba4b9513f
Filip Kapusta <filipkapusta1@gmail.com>
2016-01-08 16:10:00 +0100

    nesto

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
       :address   => 'smtp.mandrillapp.com',
       :port      => '587',
+      :enable_starttls_auto => true,
       :user_name => 'filipkapusta1@gmail.com',
       :password  => 'ShBeRdRK9HnK0ztxJbWUKQ',
       :authentication => :plain,
