8016afac130280d4bb1831e7ba710115e6552cb1
Filip Kapusta <filipkapusta1@gmail.com>
2016-01-08 16:07:20 +0100

    heroku mandrill

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -4,12 +4,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-      :address   => "smtp.mandrillapp.com",
-      :port      => 587,
-      :user_name => "filipkapusta1@gmail.com",
-      :password  => "ShBeRdRK9HnK0ztxJbWUKQ",
-      :authentication => 'login',
-      :domain => 'arg-vita.herokuapp.com'
+      :address   => 'smtp.mandrillapp.com',
+      :port      => '587',
+      :user_name => 'filipkapusta1@gmail.com',
+      :password  => 'ShBeRdRK9HnK0ztxJbWUKQ',
+      :authentication => :plain,
+      :domain => 'heroku.com'
   }
 
   ActionMailer::Base.delivery_method = :smtp
