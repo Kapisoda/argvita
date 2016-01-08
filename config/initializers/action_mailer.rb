69b1ce9370fd2445488dc9609b196e04f99c96e4
Filip Kapusta <filipkapusta1@gmail.com>
2016-01-08 15:49:40 +0100

    nesto

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -3,12 +3,14 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
       :address   => "smtp.mandrillapp.com",
       :port      => 587,
       :user_name => "filipkapusta1@gmail.com",
-      :password  => "Uyr2tw-nNPR7hvPVtRBydw",
+      :password  => "ShBeRdRK9HnK0ztxJbWUKQ",
       :authentication => 'login',
       :domain => 'heroku.com'
   }
+
+  ActionMailer::Base.delivery_method = :smtp
 end
