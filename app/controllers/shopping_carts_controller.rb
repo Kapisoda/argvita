ef17daf92aae63a863b782eed8636d197e5d2496
Filip Kapusta <filipkapusta1@gmail.com>
2016-02-05 19:12:18 +0100

    Napravljen +, - za kolicinu artikla unutar shopping carta

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -27,6 +27,67 @@ class ShoppingCartsController < ApplicationController
   end
 
   def create
+    if current_user == nil
+
+      @article = Article.find(params[:format])
+
+
+      puts "Ispred if za provjeru jel se artikl nalazi u hash-u"
+      if $no_user_articles.has_key?(@article.id.to_s)
+        $no_user_articles.each do |k, v|
+          if k == @article.id.to_s
+            $no_user_articles[k] += 1
+            $items_cost +=@article.cost
+          end
+        end
+      else
+        puts "Unutar if-else-a kada nije pronaden artikl unutar hash-a"
+        $no_user_articles[params[:format]] = 1
+        $items_cost +=@article.cost
+      end
+
+
+
+    else
+
+
+      @shopping_cart = ShoppingCart.find_by(user_id: current_user.id)
+
+      @carts_article = CartsArticle.find_by(shopping_cart_id: @shopping_cart.id, article_id: params[:format] )
+
+      if @carts_article == nil
+        CartsArticle.create(shopping_cart_id: @shopping_cart.id, article_id: params[:format], amount: 1 )
+        @carts_article = CartsArticle.find_by(shopping_cart_id: @shopping_cart.id, article_id: params[:format] )
+      else
+        @carts_article.increment!(:amount)
+      end
+    end
+
+    @article = Article.find(params[:format])
+    #@carts_articles = CartsArticle.all
+
+
+
+    if @article.on_discount.nil? || @article.on_discount == false || @article.discount != 0
+      if current_user == nil
+
+        $items_cost += @article.cost
+
+      else
+        @shopping_cart.current_cost += @article.cost
+        @shopping_cart.save
+      end
+    else
+      if current_user == nil
+
+        $items_cost += (@article.cost- (@article.cost*@article.discount/100))
+
+      end
+      @shopping_cart.current_cost += (@article.cost- (@article.cost*@article.discount/100))
+      @shopping_cart.save
+    end
+
+    redirect_to shopping_carts_show_path
 
   end
 
