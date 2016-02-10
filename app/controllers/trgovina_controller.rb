f33e42c348e996b352eba2af0daa7f0503d22da2
Filip Kapusta <filipkapusta1@gmail.com>
2016-02-10 23:36:16 +0100

    Stavljanje dizajna

--- app/controllers/trgovina_controller.rb
+++ app/controllers/trgovina_controller.rb
@@ -3,21 +3,26 @@ class TrgovinaController < ApplicationController
     @categories = Category.all
     @materials = Material.all
 
+    puts "Usao je u trgovina#index"
+
     if current_user != nil
       @shopping_cart = ShoppingCart.find_by(user_id: current_user.id)
+
+      puts "Shopping cart ID: #{@shopping_cart.id}"
+
       @carts_article = CartsArticle.find_by(shopping_cart_id: @shopping_cart.id )
     else
+      puts "NEMA USER-A!!!!"
       @articles_no_user = Article.where(id: $no_user_articles.keys)
     end
 
-    @articles = Article.all
 
     # filterific ###########################################################################################################################
-=begin
     @page_title = "Artikli"
     @filterrific = initialize_filterrific(Article.where(raw: false, for_sale: true), params[:filterrific], select_options: { sorted_by: Article.options_for_sorted_by,
                                                                                                                              with_category_id: Category.options_for_select,
                                                                                                                              with_material_id: Material.options_for_select}) or return
+
 
     @articles = @filterrific.find.page(params[:page])
 
@@ -30,7 +35,6 @@ class TrgovinaController < ApplicationController
     # There is an issue with the persisted param_set. Reset it.
     puts "Had to reset filterrific params: #{ e.message }"
     redirect_to(reset_filterrific_url(format: :html)) and return
-=end
 
     ###########################################################################################################################
 
