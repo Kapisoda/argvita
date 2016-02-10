71c69177cdadbc83d121ddfe102c374c904869dc
Filip Kapusta <filipkapusta1@gmail.com>
2016-02-10 23:29:15 +0100

    Stavljanje dizajna

--- app/controllers/trgovina_controller.rb
+++ app/controllers/trgovina_controller.rb
@@ -10,8 +10,10 @@ class TrgovinaController < ApplicationController
       @articles_no_user = Article.where(id: $no_user_articles.keys)
     end
 
+    @articles = Article.all
 
     # filterific ###########################################################################################################################
+=begin
     @page_title = "Artikli"
     @filterrific = initialize_filterrific(Article.where(raw: false, for_sale: true), params[:filterrific], select_options: { sorted_by: Article.options_for_sorted_by,
                                                                                                                              with_category_id: Category.options_for_select,
@@ -28,6 +30,7 @@ class TrgovinaController < ApplicationController
     # There is an issue with the persisted param_set. Reset it.
     puts "Had to reset filterrific params: #{ e.message }"
     redirect_to(reset_filterrific_url(format: :html)) and return
+=end
 
     ###########################################################################################################################
 
