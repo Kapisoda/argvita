94bbff825b0353f13fdbc3da7132ae779059ad29
Filip Kapusta <filipkapusta1@gmail.com>
2016-02-29 19:47:23 +0100

    Stavljen novi image slider sa thumbnail-ovima(72x72) i postavljene nove dimenzije slika, maknut pretrazivac kategorija

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -6,7 +6,7 @@ class Picture < ActiveRecord::Base
   belongs_to :single_article
 
   has_attached_file :image,
-                    :styles => {thumb: "300x300>", original: "1000x1000>", gallery: "200x200#", table: "50x50#", index: "125x200#", show: "263x362#", picture: "340x468>", thumbgal: "72x72#"}
+                    :styles => {thumb: "300x300>", original: "1000x1000>", gallery: "200x200#", table: "50x50#", index: "125x200#", show: "263x362#", picture: "340x400#", thumbgal: "72x72#"}
 
 
   do_not_validate_attachment_file_type :image
