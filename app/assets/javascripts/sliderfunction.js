b1933545ae1aa935d2dac794bfd8a14be33cf763
Filip Kapusta <filipkapusta1@gmail.com>
2016-02-29 12:44:52 +0100

    Stavljeni timer-i na popuste(trgovina/index, repromaterijal/index, home/index(root))

--- app/assets/javascripts/sliderfunction.js
+++ app/assets/javascripts/sliderfunction.js
@@ -1,16 +1,16 @@
  $(function () {
 
-        $("#range").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 0,
-            to: 5000,
-            type: 'double',
-            step: 10,
-            prefix: " kn",
-            grid: true
-        });
+     $("#range").ionRangeSlider({
+         hide_min_max: true,
+         keyboard: true,
+         min: 0,
+         max: 5000,
+         from: 0,
+         to: 5000,
+         type: 'double',
+         step: 10,
+         prefix: " kn",
+         grid: true
+     });
 
-    });
+ });
